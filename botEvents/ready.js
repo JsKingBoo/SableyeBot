a18d35468848dd1f3c245ee4d8be96576736d29c
Ghoulean <Ghoulean@gmail.com>
2017-06-03 22:06:23 -0400

    Hotfix: check if 'game' is defined before trying to change game

--- botEvents/ready.js
+++ botEvents/ready.js
@@ -1,6 +1,8 @@
 module.exports = (bot, config) => {
-	if (!bot.user.presence.game.name) {
-		bot.user.setAvatar(config.misc.avatar);
-		bot.user.setGame(config.misc.playing_game);
+	bot.user.setAvatar(config.misc.avatar);
+	if (bot.user.presence.game) {
+		if (!bot.user.presence.game.name) {
+			bot.user.setGame(config.misc.playing_game);
+		}
 	}
 }
